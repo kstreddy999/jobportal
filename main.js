5b495d420d5a599aaf5768cdba5e8245546fc08d
kstreddy999 <kstreddy999@gmail.com>
2018-10-07 17:10:08 +0530

    Job Portal 1.0.4(latest cand.)

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ function scrollFunction() {
         document.getElementById("back-to-top").style.display = "none";
     }
 }
+
+
+
+ $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin:10,
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:true
+                    },
+                    500:{
+                        items:2,
+                        nav:false
+                    },
+                    1000:{
+                        items:4,
+                        nav:true,
+                        loop:false
+                    }
+                }
+            })
